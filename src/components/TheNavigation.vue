bcc949492d507887944a1d1607735a1b2598325b
curder <q.curder@gmail.com>
2021-02-18 16:53:14 +0800

    refactor our route params

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,14 +1,72 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link :to="{ name: 'brazil' }">Brazil</router-link>
-    <router-link :to="{ name: 'hawaii' }">Hawaii</router-link>
-    <router-link :to="{ name: 'jamaica' }">Jamaica</router-link>
-    <router-link :to="{ name: 'panama' }">Panama</router-link>
+    <p class="logo">Travel App</p>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li
+        v-for="destination in destinations"
+        :key="destination.name"
+        class="links"
+      >
+        <router-link
+          :to="{ name: 'DestinationDetails', params: { id: destination.id } }"
+          v-text="destination.name"
+        ></router-link>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
+import store from "@/store";
+
 export default {
-  //
-}
+  data() {
+    return {
+      destinationId: this.$route.params.id,
+      destinations: store.destinations
+    };
+  }
+};
 </script>
+<style scoped>
+#nav {
+  display: flex;
+  align-items: center;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  border-bottom: 1px solid grey;
+  z-index: 1;
+}
+
+#nav a {
+  color: #2c3e50;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+#nav a.active-class {
+  color: #ab26ab;
+}
+
+.nav-links {
+  display: flex;
+}
+
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+
+.links:hover {
+  text-decoration: underline;
+}
+
+.logo {
+  font-size: 20px;
+  color: purple;
+  font-weight: bold;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,15 @@
     <h1>All Destination</h1>
     <div class="destinations">
       <div v-for="destination in destinations" :key="destination.name">
-        <router-link :to="{name: 'DestinationDetails', params: { id: destination.id }}">
+        <router-link
+          :to="{ name: 'DestinationDetails', params: { id: destination.id } }"
+        >
           <h2>{{ destination.name }}</h2>
         </router-link>
         <figure>
-          <router-link :to="{name: 'DestinationDetails', params: { id: destination.id }}">
+          <router-link
+            :to="{ name: 'DestinationDetails', params: { id: destination.id } }"
+          >
             <img
               :src="require(`@/assets/${destination.image}`)"
               :alt="destination.name"
@@ -44,5 +48,13 @@ img {
 .destinations {
   display: flex;
   justify-content: space-between;
+}
+a {
+  color: lightseagreen;
+  text-decoration: none;
+}
+a:hover,
+a:visited {
+  text-decoration: underline;
 }
 </style>
